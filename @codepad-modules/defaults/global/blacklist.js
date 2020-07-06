d290d0098711bf32ba0c34afce74030ad8495c80
codepad <codepad@bp-devel>
2020-07-06 03:19:43 +0200

    2.3.126

--- @codepad-modules/defaults/global/blacklist.js
+++ @codepad-modules/defaults/global/blacklist.js
@@ -1,4 +1,4 @@
 
 // blacklisted dirs are excluded from the view
 
-if (!ß.BLACKLIST_DIRS) ß.BLACKLIST_DIRS = ['data', 'datadir', 'var', 'node_modules', '.git', '.TRASH', 'log'];
+if (!ß.BLACKLIST_DIRS) ß.BLACKLIST_DIRS = ['var', 'node_modules', '.git', '.TRASH', 'log'];
