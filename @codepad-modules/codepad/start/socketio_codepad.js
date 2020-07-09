ece9d573522aad899a4c410b9b96acdefa04e24d
codepad <codepad@bp-devel>
2020-07-09 04:09:17 +0200

    2.3.126

--- @codepad-modules/codepad/start/socketio_codepad.js
+++ @codepad-modules/codepad/start/socketio_codepad.js
@@ -14,10 +14,10 @@ function add_socket_functions(socket, namespace) {
 
     ß.debug("+ " + namespace + " socket connection " + socket.username + " " + uri);
 
-    socket.on("log", function(msg) {
+    socket.on("log", function (msg) {
         console.log(msg);
     });
-    socket.on("Ł", function(msg) {
+    socket.on("Ł", function (msg) {
         Ł(msg);
     });
 
@@ -25,31 +25,29 @@ function add_socket_functions(socket, namespace) {
 
     ß.run_hook(namespace + "_socket", socket);
 
-    socket.on("disconnect", function() {
+    socket.on("disconnect", function () {
         ß.debug("- " + namespace + " socket disconnect " + socket.username + " " + uri);
     });
 }
 
 const main_io = ß.io.of("/main");
-main_io.on("connection", function(socket) {
+main_io.on("connection", function (socket) {
     add_socket_functions(socket, "main");
-  	    socket.REGISTERED = 'main';
+    socket.REGISTERED = "main";
 });
 
 const pad_io = ß.io.of("/p");
-pad_io.on("connection", function(socket) {
+pad_io.on("connection", function (socket) {
     socket.projectfile = ß.lib.pad.projectfile_by_socket(socket);
     add_socket_functions(socket, "pad");
-  	    socket.REGISTERED = 'pad';
-
+    socket.REGISTERED = "pad";
 });
 
 const log_io = ß.io.of("/log");
-log_io.on("connection", function(socket) {
+log_io.on("connection", function (socket) {
     add_socket_functions(socket, "log");
-  	    socket.REGISTERED = 'log';
-Ł();
+    socket.REGISTERED = "log";
+    ß.lib.logs.send_log();
 });
 
-ß.io.on("connection", function(socket) {
-});
+ß.io.on("connection", function (socket) {});
